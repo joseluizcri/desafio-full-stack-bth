08483710002910dc792c33ad09d41d3f0d0eaf05
joseluizcri <junior.joseluizrosa@gmail.com>
2019-02-09 13:28:58 -0200

    adicionado navbar e rotas

---
+++ src/app/pages/status/status.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StatusRoutingModule } from './status-routing.module';
+import { StatusListComponent } from './status-list/status-list.component';
+import { StatusFormComponent } from './status-form/status-form.component';
+
+@NgModule({
+  declarations: [StatusListComponent, StatusFormComponent],
+  imports: [
+    CommonModule,
+    StatusRoutingModule
+  ]
+})
+export class StatusModule { }
